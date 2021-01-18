ee2636ceb4825c99b219e94282e73e9e4d24c153
jongugli <joaoguglielmi@hotmail.com>
2021-01-18 15:18:06 -0300

    angular10

---
+++ UI/angular10/src/app/department/show-dep/show-dep.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import {SharedService} from 'src/app/shared.service';
+
+@Component({
+  selector: 'app-show-dep',
+  templateUrl: './show-dep.component.html',
+  styleUrls: ['./show-dep.component.css']
+})
+export class ShowDepComponent implements OnInit {
+
+  constructor(private service:SharedService) { }
+
+  DepartmentList:any=[];
+
+  ModalTitle:string="";
+  ActivateAddEditDepCom:boolean=false;
+  dep:any;
+
+  ngOnInit(): void {
+    this.refreshDepList();
+  }
+
+  addClick(){
+    this.dep={
+      DepartmentId:0,
+      DepartmentName:""
+    }
+    this.ModalTitle="Add Department";
+    this.ActivateAddEditDepCom=true;
+  }
+  closeClick(){
+    this.ActivateAddEditDepCom=false;
+    this.refreshDepList();
+  }
+  editClick(item:any)
+  {
+    this.dep=item;
+    this.ModalTitle = "Edit Department";
+    this.ActivateAddEditDepCom=true;
+  }
+  deleteClick(item:any){
+    if(confirm('Are you sure?')){
+      this.service.deleteDepartment(item.DepartmentId).subscribe(data=>{
+        alert(data.toString());
+        this.refreshDepList();
+      })
+    }
+  }
+  refreshDepList(){
+    this.service.getDepList().subscribe(data=>{
+      this.DepartmentList=data;
+    })
+  }
+
+}
